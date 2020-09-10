d652cd241a9ea6337090b438bef69877514abc98
Fernando H Carrion <fernandoh.carrion@gmail.com>
2020-09-10 19:33:39 -0300

    Novos testes criados a partir de outro ramo

--- src/test_area.py
+++ src/test_area.py
@@ -9,10 +9,25 @@ class TesteArea(TestCase):
         area.lado2 = 5
         self.assertEqual(25, area.quadrada())
 
+    def test_area_quadrada_deve_lancar_excecao_ao_possuir_lado_com_tamanho_zero_ou_negativo(self):
+        area = Area()
+        area.lado1 = 5
+        area.lado2 = 0
+        with self.assertRaises(ValueError):
+            area.quadrada() 
+
     def test_calcular_area_cubica(self):
         area = Area()
         area.lado1 = 2
         area.lado2 = 3
         area.lado3 = 4
         self.assertEqual(24, area.cubica())
-    
+
+    def test_area_cubica_deve_lancar_excecao_ao_possuir_lado_com_tamanho_zero_ou_negativo(self):
+        area = Area()
+        area.lado1 = 2
+        area.lado2 = 3
+        area.lado3 = -1
+        with self.assertRaises(ValueError):
+            area.cubica()
+            
