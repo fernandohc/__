bae15d2f1b87e061c0817ca03b51a9d798f622f7
Fernando H Carrion <fernandoh.carrion@gmail.com>
2020-09-10 19:35:24 -0300

    Ajustes na aplicação que atendam os novos testes

--- src/area.py
+++ src/area.py
@@ -1,9 +1,14 @@
-import unittest
 
 class Area():
 
     def quadrada(self):
-        return self.lado1 * self.lado2
+        if self.lado1 > 0 and self.lado2 > 0:
+            return self.lado1 * self.lado2
+        else:
+            raise ValueError("Nenhum dos lados pode ter valor negativo ou igual a 0")
 
     def cubica(self):
-        return self.lado1 * self.lado2 * self.lado3
+        if self.lado1 > 0 and self.lado2 > 0 and self.lado3 > 0:
+            return self.lado1 * self.lado2 * self.lado3
+        else:
+            raise ValueError("Nenhum dos lados pode ter valor negativo ou igual a 0")
